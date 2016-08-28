2223ec7799077cf030b7ac28066ece0fb47c6831
pankai <273377900@qq.com>
2016-08-28 13:18:13 +0800

    log

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -26,7 +26,7 @@ module.exports = {
 
 
 
-    var sql = `select * from user where ${where} order by id limit ${page},${num}`;
+    var sql = `select * from user where ${where} order by id desc limit ${page},${num}`;
 
     pool(sql ,query).then(function(data) {
 
